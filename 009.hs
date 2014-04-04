419f68e02ff66119f8c12996480561a756183d6b
Pavel Popov <schmooser@gmail.com>
2014-04-04 10:53:31 +0400

    9'th problem is solved

--- 009.hs
+++ 009.hs
@@ -1,4 +1,5 @@
 
+
 squares = [ j^2 | j <- [1..1000], j^2 <= 1000 ]
 triplets = [ (a, b, c) | a <- squares, b <- squares, c <- squares, a < b, b < c, a+b+c==1000 ]
 
@@ -19,15 +20,21 @@ check2 = map (\(x, y, z) -> x^2+y^2==z^2) check
  - a^2 = (1000-a-2b)(1000-a)
 -}
 
-isSquare :: (Integral a) => a -> Bool
-isSquare n = (round . sqrt $ fromIntegral n) ^ 2 == n
+(a, b) = head [ (a, b) | a <- [1..1000], b <- [1..1000], a < b, a^2==(1000-a-2*b)*(1000-a) ]
 
-a = [ a | a <- [1..1000], b <- [1..1000], a < b, a^2==(1000-a-2*b)*(1000-a) ]
+c = truncate $ sqrt $ fromIntegral (a^2+b^2)
 
+{-
+*Main> a
+200
+*Main> b
+375
+*Main> c
+425
+*Main> a+b+c
+1000
+*Main> a*b*c
+31875000
+-}
 
-b :: Double
-b = (1000-a'-a'**2/(1000-a'))
-  where a' = head a :: Double
-c = a'**2+b**2
-  where a' = head a :: Double
 
